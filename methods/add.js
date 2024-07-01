382c5eabb093a65d3a0806a973cc276b2eb7e228
gulyaasferenc <gulyaasferenc@gmail.com>
2024-07-01 18:58:34 +0200

    some formats

--- methods/add.js
+++ methods/add.js
@@ -1,36 +1,28 @@
 const rxjs = require('rxjs')
 
-const {
-    fromEvent,
-    Observable
-} = rxjs
+const { fromEvent, Observable } = rxjs
 
-module.exports = ({
-    dbName,
-    storeName,
-    values = []
-}) => {
-    const myDb = window.indexedDB.open(dbName)
+module.exports = ({ dbName, storeName, values = [] }) => {
+  const myDb = window.indexedDB.open(dbName)
 
-    const onSuccess = fromEvent(myDb, 'success')
-    const onError = fromEvent(myDb, 'error')
+  const onSuccess = fromEvent(myDb, 'success')
+  const onError = fromEvent(myDb, 'error')
 
-    onError.subscribe(error => {
+  onError.subscribe((error) => {
+    throw new Error(error)
+  })
+
+  onSuccess.subscribe((db) => {
+    const myDb = db.target.result
+    values.forEach((value) => {
+      const request = myDb
+        .transaction([storeName], 'readwrite')
+        .objectStore(storeName)
+        .add(value.value, value.key)
+      const onError = fromEvent(request, 'error')
+      onError.subscribe((error) => {
         throw new Error(error)
+      })
     })
-
-    onSuccess.subscribe(db => {
-        const myDb = db.target.result
-        values.forEach(value => {
-            const request = myDb
-                .transaction([storeName], "readwrite")
-                .objectStore(storeName)
-                .add(value.value, value.key)
-            const onError = fromEvent(request, 'error')
-            onError.subscribe(error => {
-                throw new Error(error)
-            })
-        })
-    })
-
+  })
 }
